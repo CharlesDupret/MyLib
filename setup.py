c5736918bffead6a0f235b1f43ab07c83a0bd1a6
Charles Dupret <charles.dupret@grenoble-inp.org>
2022-09-25 13:09:54 +0200

    add: config

--- setup.py
+++ setup.py
@@ -1,23 +1,4 @@
 from setuptools import setup
 
-setup(
-    name='charle_s_lib',
-    version='0.0.1',
-    description='My own library for data analysis',
-    py_modules=["charles_lib"],
-    package_dir={'': 'src'},
-    classifiers=[
-        "Programming Language :: Python ::3",
-        "Programming Language :: Python ::3.6",
-    ],
-    install_requires=[
-        "blessings ~= 1.7",
-    ],
-    extras_require={
-        "dev": [
-            "pytest>=3.7",
-        ],
-    },
-    author="Charles Dupret",
-    author_email="charles.dupret@grenoble-inp.org",
-)
+if __name__ == "__main__":
+    setup()
